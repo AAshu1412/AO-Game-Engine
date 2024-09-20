8b0a7914b234b8353f4bcc5b84fe48abdf638ddf
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-09-20 17:17:48 +0530

    resize

--- src/components/InstanceBox.jsx
+++ src/components/InstanceBox.jsx
@@ -4,17 +4,20 @@ function InstanceBox() {
     const { babylonInstanceEngine } = useBabylon();
 
     return (
-      <div className="w-[13%] h-screen bg-[#191919] border-[#2cead7] border-r-transparent text-white border-2 flex flex-row justify-center">
-       <div className="flex flex-col gap-10">
-       <h1 className="text-xl font-medium">InstanceBox</h1>
+      <div className="w-full sm:w-[20%] lg:w-[13%] h-screen bg-[#191919] border-[#2cead7] border-r-transparent text-white border-2 flex flex-row justify-center">
+       <div className="flex flex-col gap-10 p-4">
+       <h1 className="text-lg sm:text-xl font-medium">InstanceBox</h1>
        <div className="flex flex-col items-center gap-5 text-white">
-        <button className="border-2 border-[#2cead7] w-52 p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" onClick={() => babylonInstanceEngine.ground()}>
+        <button className="border-2 border-[#2cead7] w-full sm:w-40 lg:w-52 p-2 sm:p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" 
+          onClick={() => babylonInstanceEngine.ground()}>
             Ground
         </button>
-        <button className="border-2 border-[#2cead7] w-52 p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" onClick={() => { babylonInstanceEngine.box(1); }}>
+        <button className="border-2 border-[#2cead7] w-full sm:w-40 lg:w-52 p-2 sm:p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" 
+          onClick={() => { babylonInstanceEngine.box(1); }}>
             Box
         </button>
-        <button className="border-2 border-[#2cead7] w-52 p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" onClick={() => babylonInstanceEngine.sphere()}>
+        <button className="border-2 border-[#2cead7] w-full sm:w-40 lg:w-52 p-2 sm:p-3 hover:bg-red-500 active:bg-red-700 transition duration-300" 
+          onClick={() => babylonInstanceEngine.sphere()}>
             Sphere
         </button>
        </div>
